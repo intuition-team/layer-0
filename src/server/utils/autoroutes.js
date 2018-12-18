611d45a081a143ce047cf2ad0dbdb0fc4b374d84
Michael Kalygin <michael.kalygin@gmail.com>
2018-12-18 07:04:22 +0300

    Accept regexp rules for autoroutes overrides

--- src/server/utils/autoroutes.js
+++ src/server/utils/autoroutes.js
@@ -1,14 +1,21 @@
 const globby = require('globby');
 const config = require('../config');
 
-const autoroutes = (router, override = {}) => {
+const mapEndpoint = (rules, path) => {
+  const keys = Object.keys(rules);
+  const match = keys.find(key => new RegExp(key).test(path));
+
+  return match ? path.replace(new RegExp(match), rules[match]) : path;
+};
+
+const autoroutes = (router, rules = {}) => {
   if (config.autoroutes) {
     globby(config.autoroutes).then(paths => {
       paths
         .map(path => path.replace(config.views, ''))
         .map(path => path.replace(/\.[^/.]+$/, ''))
         .forEach(path => {
-          const endpoint = override[path] || path;
+          const endpoint = mapEndpoint(rules, path);
           const view = path.substring(1);
 
           router.get(endpoint, (req, res) => {
