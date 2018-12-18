7a3a7bfcffcfd7bde7ccb28ec5ff4959640b048f
Michael Kalygin <michael.kalygin@gmail.com>
2018-12-18 03:29:49 +0300

    Setup stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'stylelint-config-recommended',
+  plugins: ['stylelint-scss'],
+  rules: {
+    'at-rule-no-unknown': [true, {
+      ignoreAtRules: ['function', 'return', 'if', 'each', 'include', 'mixin'],
+    }],
+  },
+};
