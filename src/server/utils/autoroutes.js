0248ca5b3384abc8040f908bd127caf097d9165d
Michael Kalygin <michael.kalygin@gmail.com>
2019-05-21 13:00:36 +0300

    Make autoroutes strict

--- src/server/utils/autoroutes.js
+++ src/server/utils/autoroutes.js
@@ -2,32 +2,37 @@ const path = require('path');
 const globby = require('globby');
 const config = require('../config');
 
-const mapEndpoint = (rules, filename) => {
+const mapRelativeFilename = filename =>
+  path
+    .resolve(filename)
+    .replace(config.views, '')
+    .replace(/\.[^/.]+$/, '')
+    .replace(/\\/g, '/');
+
+const mapRoute = rules => filename => {
   const keys = Object.keys(rules);
   const match = keys.find(key => new RegExp(key).test(filename));
+  const view = filename.substring(1);
+  const endpoint = match
+    ? filename.replace(new RegExp(match), rules[match])
+    : filename;
 
-  return match ? filename.replace(new RegExp(match), rules[match]) : filename;
+  return { view, endpoint };
+};
+
+const createRoute = router => ({ view, endpoint }) => {
+  router.get(`${endpoint}$`, (req, res) => {
+    res.render(view);
+  });
 };
 
 const autoroutes = (router, rules = {}) => {
   if (config.autoroutes) {
     globby(config.autoroutes).then(filenames => {
       filenames
-        .map(filename =>
-          path
-            .resolve(filename)
-            .replace(config.views, '')
-            .replace(/\.[^/.]+$/, '')
-            .replace(/\\/g, '/')
-        )
-        .forEach(filename => {
-          const endpoint = mapEndpoint(rules, filename);
-          const view = filename.substring(1);
-
-          router.get(endpoint, (req, res) => {
-            res.render(view);
-          });
-        });
+        .map(mapRelativeFilename)
+        .map(mapRoute(rules))
+        .forEach(createRoute(router));
     });
   }
 };
