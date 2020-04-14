5f8f046a245ddb1315ba8526ad1cb4f20c045748
Vadim S <vadim.stebakov@gmail.com>
2020-04-14 18:25:25 +0300

    Remove unnecessary files

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,8 +2,11 @@ module.exports = {
   extends: 'stylelint-config-recommended',
   plugins: ['stylelint-scss'],
   rules: {
-    'at-rule-no-unknown': [true, {
-      ignoreAtRules: ['function', 'return', 'if', 'each', 'include', 'mixin'],
-    }],
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['function', 'return', 'if', 'each', 'include', 'mixin'],
+      },
+    ],
   },
 };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,1 @@
 import '../styles/main.scss';
-import './bubbles';
