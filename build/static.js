b1a44faa8a7b3a2ac40cc7415d255695279bbb75
Vadim S <vadim.stebakov@gmail.com>
2020-04-20 13:45:34 +0300

    Fix

--- build/static.js
+++ build/static.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const pug = require('pug');
-const fs = require('fs');
+const fs = require('fs-extra');
 const globby = require('globby');
 const config = require('../src/server/config');
 const { assetPath } = require('../src/server/helpers/asset');
@@ -11,24 +11,52 @@ const outputDir = path.resolve(__dirname, '..', 'site');
 const mapRelativeFilename = filename => path.basename(filename, '.pug');
 
 const compileHtmlFile = fileName => {
-  const compileToFunction = pug.compileFile(`${inputDir}/${fileName}.pug`, {
+  const template = pug.compileFile(path.resolve(inputDir, `${fileName}.pug`), {
     basedir: inputDir,
     pretty: true,
   });
-  const html = compileToFunction({
+  const html = template({
     req: { originalUrl: '' },
     assetPath,
   });
 
-  fs.writeFile(`${outputDir}/${fileName}.html`, html, err => {
+  fs.writeFile(path.resolve(outputDir, `${fileName}.html`), html, err => {
     if (err) throw err;
   });
 };
 
-if (!fs.existsSync(outputDir)) {
-  fs.mkdirSync(outputDir);
+if (fs.existsSync(outputDir)) {
+  fs.removeSync(outputDir);
 }
+
+fs.mkdirSync(outputDir);
 
 globby(config.autoroutes).then(names =>
   names.map(mapRelativeFilename).forEach(compileHtmlFile)
+);
+
+fs.copy(
+  path.resolve(__dirname, '..', 'static'),
+  path.resolve(__dirname, '..', 'site'),
+  err => {
+    if (err) {
+      throw err;
+    } else {
+      fs.readdir(
+        path.resolve(__dirname, '..', 'site', 'assets'),
+        (err, files) => {
+          files.forEach(file => {
+            const m = file.match(/\.([a-z, A-Z, 0-9]){8}\./);
+            if (m) {
+              const name = file.replace(m[0], '.');
+              fs.renameSync(
+                path.resolve(__dirname, '..', 'site', 'assets', file),
+                path.resolve(__dirname, '..', 'site', 'assets', name)
+              );
+            }
+          });
+        }
+      );
+    }
+  }
 );
