5ee220ddcbb632c5e4d2167f023d4e1fec21eaeb
Michael Kalygin <michael.kalygin@gmail.com>
2020-04-21 00:33:07 +0300

    Improve static site generation and fix static files copy

--- build/static.js
+++ build/static.js
@@ -2,61 +2,48 @@ const path = require('path');
 const pug = require('pug');
 const fs = require('fs-extra');
 const globby = require('globby');
+const { ncp } = require('ncp');
 const config = require('../src/server/config');
-const { assetPath } = require('../src/server/helpers/asset');
+const helpers = require('../src/server/helpers');
 
-const inputDir = config.views;
-const outputDir = path.resolve(__dirname, '..', 'site');
+const viewsDir = config.views;
+const staticDir = path.resolve(__dirname, '..', 'static');
+const siteDir = path.resolve(__dirname, '..', 'site');
 
-const mapRelativeFilename = filename => path.basename(filename, '.pug');
-
-const compileHtmlFile = fileName => {
-  const template = pug.compileFile(path.resolve(inputDir, `${fileName}.pug`), {
-    basedir: inputDir,
-    pretty: true,
-  });
-  const html = template({
-    req: { originalUrl: '' },
-    assetPath,
-  });
-
-  fs.writeFile(path.resolve(outputDir, `${fileName}.html`), html, err => {
-    if (err) throw err;
-  });
+const pugOptions = {
+  basedir: viewsDir,
+  pretty: true,
 };
 
-if (fs.existsSync(outputDir)) {
-  fs.removeSync(outputDir);
+const getLocals = () => {
+  const req = { url: '' };
+  const res = { locals: {} };
+  const next = () => {};
+
+  helpers()(req, res, next);
+
+  return res.locals;
+};
+
+const compilePugToHtml = pugPath => {
+  const template = pug.compileFile(pugPath, pugOptions);
+  const html = template(getLocals());
+  const fileName = path.basename(pugPath, '.pug');
+  const htmlPath = path.resolve(siteDir, `${fileName}.html`);
+
+  fs.writeFileSync(htmlPath, html);
+};
+
+// Remove stale site's build.
+if (fs.existsSync(siteDir)) {
+  fs.removeSync(siteDir);
 }
 
-fs.mkdirSync(outputDir);
+// Create site's directory.
+fs.mkdirSync(siteDir);
 
-globby(config.autoroutes).then(names =>
-  names.map(mapRelativeFilename).forEach(compileHtmlFile)
-);
+// Build pug files to html files.
+globby(config.autoroutes).then(pugPaths => pugPaths.forEach(compilePugToHtml));
 
-fs.copy(
-  path.resolve(__dirname, '..', 'static'),
-  path.resolve(__dirname, '..', 'site'),
-  err => {
-    if (err) {
-      throw err;
-    } else {
-      fs.readdir(
-        path.resolve(__dirname, '..', 'site', 'assets'),
-        (err, files) => {
-          files.forEach(file => {
-            const m = file.match(/\.([a-z, A-Z, 0-9]){8}\./);
-            if (m) {
-              const name = file.replace(m[0], '.');
-              fs.renameSync(
-                path.resolve(__dirname, '..', 'site', 'assets', file),
-                path.resolve(__dirname, '..', 'site', 'assets', name)
-              );
-            }
-          });
-        }
-      );
-    }
-  }
-);
+// Copy static files.
+ncp(staticDir, siteDir);
